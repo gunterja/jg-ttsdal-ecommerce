f2a22ee50968865460980cc16dcd266cc73ab670
Jeremy Gunter <gunterja@gmail.com>
2015-06-01 16:34:58 +0000

    Added a whole lot of shit. Made changes to CSS and added seed information.

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,35 +10,41 @@ Product.delete_all
 Product.create!(
   title:"Marker",
   description:" Best marker on the market! ",
+  image_url: "http://www.rainbowracing.com/onlinestore/sites/default/files/imagecache/product_full/item_368_Marks-A-Lot_Jumbo_Markers-01.png",
   price: 1.29
 )
 
 Product.create!(
   title:"Mechanical Pencil",
   description:" Click to extend lead, no sharpening needed! ",
+  image_url: "http://cdn.dickblick.com/items/206/03/20603-7005-1-2ww-m.jpg",
   price: 3.99
 )
 
 Product.create!(
   title:"Spiral Notebook",
   description:" Keepp all your notes neatly organized. ",
+  image_url: "http://ecx.images-amazon.com/images/I/41N1dVObzTL._SY300_.jpg",
   price: 0.99
 )
 
 Product.create!(
   title:"Project Folder",
   description:" A nice pocketed folder to keep your loose sheets protected. ",
+  image_url: "http://ecx.images-amazon.com/images/I/41FfbkQx28L._AA160_.jpg",
   price: 2.59
 )
 
 Product.create!(
   title:"Pencil Box",
   description:" A small plastic box to hold all of your writing utensils. ",
+  image_url: "http://bestinclassschoolsupplies.com/images/20603.jpg",
   price: 1.99
 )
 
 Product.create!(
   title:"Book Bag",
   description:" Small nylon, drawstring bag for carrying all of your supplies! ",
+  image_url: "http://weiskamp.com/wp-content/themes/weiskamp/productimages/31962_f_fl.jpg",
   price: 9.99
 )
